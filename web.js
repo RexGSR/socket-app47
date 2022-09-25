92e2fa9f276832ae8c57bc0d9e06350b1c3a8254
gaurav.rana1 <gaurav.rana@coderootz.com>
2022-09-25 11:54:36 +0530

    modification for cors

--- web.js
+++ web.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const cors = require("cors");
+
+
 
 let https = require('https').createServer(app);
 const io = require('socket.io')(https, {
     cors: {
-        origins: process.env.CORS_URLS.split(", "),
+        origin: "*",
     },
 });
 
@@ -15,17 +18,20 @@ const io = require('socket.io')(https, {
 const PORT = process.env.PORT || 3000;
 
 
-
+app.use(cors({
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+}));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json()); 
 app.use(express.static(path.join(__dirname, 'public')));
 app.get('/',(req, res) => res.send('Hello World'));
-app.get('/', function (req, res) {
+app.get('/index', function (req, res) {
     res.sendFile(path.join(__dirname, '/index.html'));
 });
 
 https.listen(PORT, function () {
-    console.log('App listening at http://%s:%s', host, port)
+    console.log('App listening on', PORT)
 });
 io = new Server(httpServer, {
     maxHttpBufferSize: 1024,
